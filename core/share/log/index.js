3620a1fae7754c87ffdd9af6ead4b359c2494715
chendf <1725760203@qq.com>
2024-07-17 21:11:32 +0800

    feat: 修改log文件

--- core/share/log/index.js
+++ core/share/log/index.js
@@ -1,18 +1,18 @@
 class Log {
     static info(message, ...args) {
-        console.log(`[info]: ${message}`, ...args)
+        console.log(`\x1b[34m[info]\x1b[0m: ${message}`, ...args)
     }
 
     static error(message, ...args) {
-        console.error(`[error]: ${message}`, ...args)
+        console.error(`\x1b[31m[error]\x1b[0m: ${message}`, ...args)
     }
 
     static warn(message, ...args) {
-        console.warn(`[warn]: ${message}`, ...args)
+        console.warn(`\x1b[33m[warn]\x1b[0m: ${message}`, ...args)
     }
 
     static success(message, ...args) {
-        console.log(`[success]: ${message}`, ...args)
+        console.log(`\x1b[32m[success]\x1b[0m: ${message}`, ...args)
     }
 }
 
