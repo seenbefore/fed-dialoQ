17a03e15f25593c7d9e212a297d7281d32f85aca
chendf <1725760203@qq.com>
2024-07-19 09:58:11 +0800

    feat: 修复下载的core文件夹位置不对

--- core/developmentTools/updateCore/index.js
+++ core/developmentTools/updateCore/index.js
@@ -1,5 +1,5 @@
 const { join } = require('path')
-const { existsSync, mkdirSync, rmdirSync } = require('fs')
+const { existsSync, mkdirSync } = require('fs')
 const { Log } = require('../../share/log/index.js')
 const { extractFromRemote, defaultRemoteUrl } = require('../../utils/gitHelpers.js')
 const { baseRootPath, temporaryPath } = require('../../utils/path.js')
@@ -10,13 +10,12 @@ const { removeDirSync } = require('../../utils/file')
 const args = process.argv.slice(2)
 const [force] = args
 
-const coverPath = join(baseRootPath, 'core')
-
 const cover = async () => {
-    await extractFromRemote(defaultRemoteUrl, 'master', 'core', coverPath)
+    await extractFromRemote(defaultRemoteUrl, 'master', 'core', baseRootPath)
 }
 
 const updateCore = async () => {
+    let updateConfig = true
     /* npm run update:core force */
     /* 不管版本强制更新core文件夹 */
     if (force === 'force') {
@@ -34,20 +33,24 @@ const updateCore = async () => {
         /* remote version */
         const { version } = require(join(temporaryPath, 'core/config.json'))
         /* local version */
-        const { version: localVersion } = require(join(coverPath, 'config.json'))
+        const { version: localVersion } = require(join(baseRootPath, 'core/config.json'))
         /* 版本格式为 x.x.x x 为数字，所以可以直接使用运算符对比。 */
         if (version === localVersion) {
             Log.warn(`版本相同，无需更新。当前版本：${localVersion}。如需强制更新可执行：npm run update:core force`)
+            updateConfig = false
         } else if (localVersion > version) {
             // 可能修改了config的version？或者远程的config不对
             Log.warn(`本地版本大于远程版本，无法更新。本地版本：${localVersion}，远程版本：${version}`)
+            updateConfig = false
         } else {
             Log.info('开始更新core文件内容，请稍等')
             await cover()
             Log.success('core文件更新成功')
         }
     }
-    await inspectConfig()
+    if (updateConfig) {
+        await inspectConfig()
+    }
     /* 删除临时文件夹 */
     if (existsSync(temporaryPath)) {
         await removeDirSync(temporaryPath)
