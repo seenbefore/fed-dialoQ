bbf0a097a9b4cdd3fc8464c1ad44ba844b904bff
hejiaxiang <hejiaxiang@icinfo.com.cn>
2024-12-03 08:56:12 +0800

    feat: 更改通用封面组件

--- src/views/file-review/components/case-step-cover/index.vue
+++ src/views/file-review/components/case-step-cover/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
+        <!-- {{ templateConfigKeys }} -->
         <DocInput
             ref="docInputRef"
             :doc-params="docParams"
+            :cus-doc-form-data="cusDocFormData"
             :custom-get-doc-base-info="customGetDocBaseInfo"
             :custom-get-doc-form="customGetDocForm"
-            :get-params-doc-form="getParamsDocForm"
             is-custom-save-http
-            @emitDataMap="emitDataMap"
         ></DocInput>
     </div>
 </template>
@@ -15,18 +15,6 @@
 <script lang="tsx">
 import { Component, Vue, Prop, Watch, Ref } from 'vue-property-decorator'
 import DocInput from '@/components/doc-input/index.vue'
-import { getDocBaseInfo } from '@/services/auto/common/volume'
-import { firstSave, getArchiveVolumeRecordById } from '@/services/auto/my/volume'
-import { DocumentCommonFormHtmlVo } from '@/components/doc-input/service/punish/csource/common'
-import { useLoading } from '@/hooks/useLoading'
-import { getCaseNumberConfigList, ArchiveCaseNumberConfigVO } from '@/services/auto/config/caseNumber'
-/**1:格式不可编辑,2:格式可编辑 */
-export enum CaseNumberLimit {
-    /** 不可编辑 */
-    UNEDITABLE = '1',
-    /** 可编辑 */
-    EDITABLE = '2',
-}
 /** 卷宗封面 */
 export interface CaseStepCoverClass {
     /** 获取表单数据 */
@@ -40,24 +28,15 @@ export interface CaseStepCoverClass {
     },
 })
 export default class Step1 extends Vue {
-    /**机构编码 */
-    @Prop({ type: String, default: '' }) orgCode!: string
-    /**条线编码 */
-    @Prop({ type: String, default: '' }) lineCode!: string
-    /**卷宗类型编码 */
-    @Prop({ type: String, default: '' }) volumeTypeCode!: string
-    @Prop({ type: Boolean, default: false }) isEditVolume!: boolean
+    /**
+     * 文书数据
+     */
     @Prop({
-        default: () => ({
-            partyId: '',
-            documentCatalogCode: '',
-            documentId: '',
-            operateType: '',
-            documentTemplateCode: '',
-            templateCode: 'DT2DZJZFM0000000001',
-            surveyPeopleId: '',
-            caseId: '',
-        }),
+        default: () => ({}),
+    })
+    row!: any
+    @Prop({
+        default: () => ({}),
     })
     docParams!: any
     mounted() {
@@ -65,87 +44,47 @@ export default class Step1 extends Vue {
     }
     @Ref('docInputRef')
     docInputRef!: DocInput
-    /**自定义获取文书表单数据 */
-    get customGetDocForm() {
-        return async (params: any) => {
-            const { data } = await getArchiveVolumeRecordById(params)
-            const getCaseNumberConfigData = await this.getCaseNumberConfigList()
-            return {
-                data: {
-                    dataMap: {
-                        ...data,
-                        fondNumber: data.fondNumber ?? getCaseNumberConfigData.fondNumber ?? '',
-                        catalogNumber: data.catalogNumber ?? getCaseNumberConfigData.catalogNumber ?? '',
-                        caseFileNumber: data.volumeNumber ?? '',
-                    },
-                },
-            }
-        }
+    cusDocFormData = {
+        caseAddress: '22',
     }
-    /**自定义获取文书模板数据 */
-    get customGetDocBaseInfo() {
-        return async () => {
-            const res = await getDocBaseInfo(this.docParams)
-            const data = await this.handleDocBaseInfo(res.data)
-            return {
-                ...res,
-                data,
-            }
-        }
-    }
-    get getParamsDocForm() {
-        return {
-            volumeRecordId: this.$route.query.id,
-        }
+    templateConfigKeys: any[] = []
+    public handleCancel() {
+        console.log('取消222')
     }
     public async save() {
-        await this.docInputRef.saveData()
+        console.log('step1 save')
+        const result: any = await this.docInputRef.saveData()
+        console.log('result', result)
+        return result.values
     }
     public async preview() {
         console.log('step1 next')
     }
-
-    /**处理配置项 */
-    async handleDocBaseInfo(configInfo: DocumentCommonFormHtmlVo) {
-        const getCaseNumberConfigData = await this.getCaseNumberConfigList()
-        const { templateConfigMap = {} } = configInfo
-        // 设置全宗号 和 目录号 是否可编辑
-        const editVolumeFields = ['fondNumber', 'catalogNumber']
-        Object.keys(templateConfigMap).forEach(key => {
-            if (editVolumeFields.includes(key)) {
-                if (getCaseNumberConfigData[key + 'Limit'] === CaseNumberLimit.EDITABLE && templateConfigMap[key].controlConfigContent) {
-                    const controlConfigContent = {
-                        ...JSON.parse(templateConfigMap[key].controlConfigContent),
-                        isEdit: '1',
-                    }
-                    templateConfigMap[key].controlConfigContent = JSON.stringify(controlConfigContent)
-                }
+    /**自定义获取文书模板的接口 */
+    get customGetDocBaseInfo() {
+        return () =>
+            this.$http.post('/common/volume/getDocBaseInfo', this.docParams).then(res => {
+                this.templateConfigKeys = Object.keys(res.data?.templateConfigMap || {})
+                return res
+            })
+    }
+    /**自定义获取文书表单的接口 */
+    get customGetDocForm() {
+        const templateConfigKeys = this.templateConfigKeys
+        if (this.docParams.volumeRecordId) {
+            return () => this.$http.post('/my/volume/getArchiveVolumeRecordById', this.docParams)
+        }
+        return () => {
+            const data = templateConfigKeys.reduce((acc, key) => {
+                acc[key] = this.row[key]
+                return acc
+            }, {})
+            return {
+                data: {
+                    ...data,
+                },
             }
-        })
-        return configInfo
-    }
-    /**获取封面底部编号配置 */
-    async getCaseNumberConfigList(): Promise<ArchiveCaseNumberConfigVO> {
-        let caseNumberConfig = {}
-        try {
-            const { data } = await getCaseNumberConfigList({ orgCode: this.orgCode, lineCode: this.lineCode, volumeTypeCode: this.volumeTypeCode }, { exNoErrorMassage: true })
-            caseNumberConfig = data?.[0] ?? {}
-        } catch (error) {
-            console.log(error)
         }
-        return caseNumberConfig as ArchiveCaseNumberConfigVO
     }
-    /**保存文书 */
-    async emitDataMap(tabIndex, values, sendData, resolve) {
-        if (tabIndex === -1) {
-            return
-        }
-        await useLoading(firstSave, {
-            ...(sendData.dataMap || {}),
-            id: this.$route.query.id,
-        })
-        resolve(true)
-    }
-    created() {}
 }
 </script>
